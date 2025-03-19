061226f2572444e31886b3c3e69843b3fb9f0d99
Salami Azeez <salamitopeazeez@gmail.com>
2025-03-19 18:12:25 +0100

    Added Navbar layout

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,42 @@
-
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const navLinks = [
+        { name: "Home", link: "/" },
+        { name: "About", link: "/about" },
+        { name: "Services", link: "/services" }
+    ]
+    return (
+        <nav
+            className={`bg-darkBlue px-20 sticky flex items-center
+                h-[70px] justify-between items- text-white 
+            `}
+        >
+            {/* Nav links */}
+            <section className="">
+                <ul className="flex gap-10">
+                    {
+                        navLinks.map((link, i) => (
+                            <li className="" key={i}>
+                                {/* <Link to={link.link}>{link.name}</Link> */}
+                                <a href={link.link}>{link.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+            {/* ==== LOGO ==== */}
+            <section>
+                <h3>
+                    <span>inks &</span>
+                    <span>pixels</span>
+                </h3>
+            </section>
+            {/* ==== CONTACT ==== */}
+            <section>
+                <button>Contact</button>
+            </section>
+        </nav>
+
+    )
 }
