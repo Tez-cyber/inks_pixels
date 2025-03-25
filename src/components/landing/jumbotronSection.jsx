cd89fd53d8456d0ac1eacafeba394bd8f8d8cc74
Salami Azeez <salamitopeazeez@gmail.com>
2025-03-25 14:57:59 +0100

    Made jumboSection responsive

--- src/components/landing/jumbotronSection.jsx
+++ src/components/landing/jumbotronSection.jsx
@@ -12,19 +12,19 @@ import { Button } from '../button';
 import { Container } from '../container';
 
 export const JumbotronSection = () => {
-    const imgStyle = 'mb-6 h-[300px] w-[319.25px] rounded-3xl object-cover'
+    const imgStyle = 'mb-6 h-[170px] w-[319.25px] rounded-3xl object-cover md:h-[300px]'
 
     return (
-        <section className='bg-darkBlue w-full h-full flex items-center overflow-hidden md:h-screen'>
+        <section className='bg-darkBlue w-full h-[916.31px] flex items-center overflow-hidden md:h-screen'>
             <Container className="flex flex-col items-center w-full md:flex-row">
                 {/* ====== Description section ====== */}
-                <div className=' text-white flex flex-col gap-6 md:w-[50%]'>
-                    <h1 className="text-[54px] leading-tight md:text-[54px  ]">
+                <div className=' text-white bg-darkBlue flex flex-col gap-6 mt-[7rem] pb-[4rem] md:mt- md:w-[50%] lg:mt-0'>
+                    <h1 className="text-[40px] leading-tight md:text-[36px] lg:text-[50px]">
                         Empowering Your <br />
                         Brand with
                         <span className='ml-3 text-gold'>Creative Excellence</span>
                     </h1>
-                    <p className='text-[18px] w-[85%]'>
+                    <p className='w-[85%] md:text-[18px]'>
                         At Ink and Pixels Nigeria, we transform your vision into reality through
                         innovative design and branding solutions. Let us help you stand out in a
                         crowded marketplace.
@@ -45,7 +45,7 @@ export const JumbotronSection = () => {
                     </div>
                 </div>
                 {/* ===== Image Section */}
-                <div className='flex justify-center md:w-[50%]'>
+                <div className='flex justify-center h-[460.31px] overflow-hidden md:h-auto md:w-[50%]'>
                     <div className='grid grid-cols-2 gap-6'>
                         {/* ==== First grid ==== */}
                         <div>
@@ -60,7 +60,7 @@ export const JumbotronSection = () => {
                             </div>
                         </div>
                         {/* ==== Second grid ==== */}
-                        <div className='mt-36'>
+                        <div className='-mt-14 md:mt-36'>
                             <div className=''>
                                 <img className={`${imgStyle}`} src={gridImage5} alt="gridImage1" />
                             </div>
