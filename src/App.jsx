263390f2152049b5df8e62b34a66192d1ace4c8f
Salami Azeez <salamitopeazeez@gmail.com>
2025-03-28 17:43:22 +0100

    Imported footer in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/navbar/navbar"
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
+import { Footer } from "./components/footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
+      <Footer />
     </>
   )
 }
