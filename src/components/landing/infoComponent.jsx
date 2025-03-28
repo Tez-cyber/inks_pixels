2ed5fd26a92fca313e0b72252309d7a03a1cb233
Salami Azeez <salamitopeazeez@gmail.com>
2025-03-28 11:33:06 +0100

    Added infoComponent file

---
+++ src/components/landing/infoComponent.jsx
@@ -0,0 +1,45 @@
+import { Container } from "../container"
+import aboutImg from '../../assets/img-1.jpg';
+import { Button } from "../button";
+
+export const InfoComponent = ({
+    bgColor, infoTitle, infoDesc,
+    imgSrc, imgAlt
+}) => {
+    return (
+        <section className={bgColor}>
+            <Container className="flex flex-col gap-6 py-20 md:items-center md:flex-row">
+                {/* ====== DESC SECTION ===== */}
+                <section className=' text-darkBlue flex flex-col gap-6 md:w-[50%]'>
+                    <h1 className="text-[35px] leading-tight md:text-[36px] lg:text-[40px]">
+                        {infoTitle}
+                    </h1>
+                    <p className='text-[14px] md:w-[75%] md:text-[18px]'>
+                        {infoDesc}
+                    </p>
+                    {
+                        buttonText ?
+                            (
+                                <div className='flex gap-3'>
+                                    <Button
+                                        size="md"
+                                        variant="secondary"
+                                        className="transition-all duration-150 hover:bg-darkBlue hover:text-white hover:border-lightBlue active:border-0"
+                                    >
+                                        {buttonText}
+                                    </Button>
+                                </div>
+                            ) : ("")
+                    }
+                </section>
+                {/* ====== IMAGE SECTION ===== */}
+                <section className="md:w-[50%]">
+                    <div className="">
+                        <img className="w-[500px] h-[500px] rounded-3xl object-cover" src={imgSrc} alt={imgAlt} />
+                    </div>
+                </section>
+            </Container>
+        </section>
+    )
+}
+
