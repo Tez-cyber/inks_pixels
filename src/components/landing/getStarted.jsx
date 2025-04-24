fd7e5f939d378a88b158ec9834ead33617e81626
Salami Azeez <salamitopeazeez@gmail.com>
2025-04-24 11:39:30 +0100

    change background for getStarted section

--- src/components/landing/getStarted.jsx
+++ src/components/landing/getStarted.jsx
@@ -4,11 +4,11 @@ import { Button } from '../button'
 
 export const GetStarted = () => {
     return (
-        <section className='bg-veryLightBlue'>
+        <section className='bg-lightBlue'>
             <Container className="text-center py-20">
-                <div className='border-[1.5px] border-[#aea9c5] rounded-2xl py-10 px-5 flex flex-col items-center justify-center gap-10'>
-                    <h1 className='text-3xl text-darkBlue md:text-5xl'>Let's Bring Your Vision to Life</h1>
-                    <p className='text-lightBlue'>
+                <div className='border-[1.5px] border-[#aea9c5] text-veryLightBlue rounded-2xl py-10 px-5 flex flex-col items-center justify-center gap-10'>
+                    <h1 className='text-3xl md:text-5xl'>Let's Bring Your Vision to Life</h1>
+                    <p className=''>
                         Contact us today for a personalized <br className='md:hidden' />
                         consultation or quote tailored to your <br className='md:hidden' />
                         unique needs.
