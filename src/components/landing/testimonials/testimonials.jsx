db8c0090c4ad5ae389c290b5dcdd22437472c006
Salami Azeez <salamitopeazeez@gmail.com>
2025-04-25 10:48:48 +0100

    Made testimonial section responsive

--- src/components/landing/testimonials/testimonials.jsx
+++ src/components/landing/testimonials/testimonials.jsx
@@ -43,9 +43,9 @@ export const Testimonials = () => {
     ]
     return (
         <section>
-            <div className="flex items-center w-full p-10 h-[80vh]">
+            <div className="flex flex-col items-center w-full p-10 md:h-[90vh] md:flex-row">
                 <TestimonialSlider testimonials={testimonials} />
-                <div className="h-full w-[60%] bg-darkBlue text-white flex flex-col justify-center gap-6 p-10">
+                <div className="h-full bg-darkBlue text-white flex flex-col justify-center gap-6 p-10 md:w-[60%]">
                     <h2 className="font-semibold text-gold text-xl">Start your project</h2>
                     <p className="leading-loose">
                         At Ink and Pixels Nigeria Limited, we're your leading printing partner in 
