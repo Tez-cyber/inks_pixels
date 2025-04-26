d37b62a777cae99812464a745afcf06af01a74df
Salami Azeez <salamitopeazeez@gmail.com>
2025-04-27 00:47:52 +0100

    Page in construction for get started page

--- src/pages/GetStartedPage.jsx
+++ src/pages/GetStartedPage.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
 
 
 export const GetStartedPage = () => {
-  return (
-    <div>GetStartedPage</div>
-  )
+  const [inProgress, setInProgress] = useState(false)
+    return (
+      <div>
+        {
+          inProgress ? (
+            <></>
+          ) : (
+            <div className="h-[50vh] bg-lightBlue text-6xl px-5 flex items-center justify-center text-white">
+              Page under construction
+            </div>
+          )
+        }
+      </div>
+    )
 }
