9debcedfe53a8b7f14571eb7dfb575a77519f11e
Salami Azeez <salamitopeazeez@gmail.com>
2025-04-28 15:37:47 +0100

    Adjusted product Section

--- src/components/landing/productSection.jsx
+++ src/components/landing/productSection.jsx
@@ -6,25 +6,25 @@ export const ProductSection = () => {
         <section className="bg-lightBlue">
             <div className="px-10 py-20">
                 <div className="pb-10">
-                    <h1 className="text-3xl font-bold text-white">Popular Products</h1>
+                    <h1 className="text-3xl font-semiboldbold text-white">Popular Products</h1>
                 </div>
                 {/* ====== product section ====== */}
                 <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     {
                         products.map((product) => {
                             return (
-                                <div key={product.id} className="flex flex-col items-center justify-center bg-veryLightBlue mt-4 overflow-hidden ">
+                                <div key={product.id} className="flex flex-col items-center justify-center mt-4 overflow-hidden ">
                                     <div className="overflow-hidden">
                                         <img
                                             src={`${product.img}`}
                                             alt={product.title}
-                                            className="h-[200px] object-cover transition-transform duration-500 ease-in-out hover:scale-105 w-[300px] md:w-[280px]"
+                                            className="h-[300px] w-[300px] object-cover rounded-2xl transition-transform duration-1000 ease-in-out hover:scale-125 md:w-[280px]"
                                         />
                                     </div>
                                     <div className="p-4 w-full md:w-full lg:w-full">
-                                        <h2 className="text-darkBlue font-semibold">{product.title}</h2>
-                                        <p className="text-sm text-gray-700">
-                                            <span className="font-semibold">Price</span>: &#8358; {product.price}
+                                        <h2 className="text-white font-semibold">{product.title}</h2>
+                                        <p className="text-sm text-gray-100">
+                                            &#8358; {product.price}
                                         </p>
                                     </div>
                                 </div>
