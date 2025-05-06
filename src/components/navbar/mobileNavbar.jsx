01acb82e4eaca252d0cd26001c23652ac48afbcb
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-06 17:50:48 +0100

    Adjusted mobileNav

--- src/components/navbar/mobileNavbar.jsx
+++ src/components/navbar/mobileNavbar.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { navLinks } from '../../utils';
 import { Button } from '../button';
 import { motion } from 'framer-motion'; // Import motion
+import { Link } from 'react-router-dom'; // Import Link for React Router
 
 const mobileNavbarVariants = {
   open: { x: 0, transition: { type: 'tween', duration: 0.3 } },
   closed: { x: '100vw', transition: { type: 'tween', duration: 0.3 } },
+};
+
+const listItemVariants = {
+  initial: { opacity: 0, y: -20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.1 } },
+  whileHover: { scale: 1.05 },
+  transition: { duration: 0.2 },
 };
 
 export const MobileNavbar = ({ onCloseNav }) => {
@@ -19,68 +27,30 @@ export const MobileNavbar = ({ onCloseNav }) => {
     >
       <motion.section className="w-full h-full absolute inset-0 flex flex-col justify-center">
         <motion.ul className="mx-16 leading-loose md:mt-4 md:leading-snug">
-          {/* {navLinks.map((nav, i) => (
+          {navLinks.map((nav, i) => (
             <motion.li
               key={i}
               className="overflow-hidden"
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.2 }}
+              variants={listItemVariants}
+              initial="initial"
+              animate="animate"
             >
-                <a
-                  href={nav.link}
-                  onClick={onCloseNav}
-                  className="text-white text-[40px] block uppercase transition duration-150 ease-in hover:text-brightGreen hover:-translate-y-1 md:text-[75px]"
-                >
-                  {nav.name}
-                </a>
+              <Link
+                to={nav.link}
+                onClick={onCloseNav}
+                className="text-white text-[40px] block uppercase transition duration-150 ease-in hover:text-brightGreen hover:-translate-y-1 md:text-[75px]"
+              >
+                {nav.name}
+              </Link>
             </motion.li>
-          ))} */}
-          <motion.li
-            className="overflow-hidden"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-          >
-            <a
-              href="/"
-              onClick={onCloseNav}
-              className="text-white text-[40px] block uppercase transition duration-150 ease-in hover:text-brightGreen hover:-translate-y-1 md:text-[75px]"
-            >
-              Home
-            </a>
-          </motion.li>
-          <motion.li
-            className="overflow-hidden"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-          >
-            <a
-              href="/about"
-              onClick={onCloseNav}
-              className="text-white text-[40px] block uppercase transition duration-150 ease-in hover:text-brightGreen hover:-translate-y-1 md:text-[75px]"
-            >
-              About
-            </a>
-          </motion.li>
-          <motion.li
-            className="overflow-hidden"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-          >
-            <a
-              href="/services"
-              onClick={onCloseNav}
-              className="text-white text-[40px] block uppercase transition duration-150 ease-in hover:text-brightGreen hover:-translate-y-1 md:text-[75px]"
-            >
-              Services
-            </a>
-          </motion.li>
+          ))}
         </motion.ul>
         <Button
           size="sm"
           variant="primary"
           className="w-fit mx-16 mt-5"
           href="/getstarted"
-          onClick={onCloseNav}
+          onClick={onCloseNav} 
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
         >
