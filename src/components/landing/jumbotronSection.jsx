568952b8fec24a29d558cc8bd02178ad7118a17a
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-16 12:31:57 +0100

    Added hero img

--- src/components/landing/jumbotronSection.jsx
+++ src/components/landing/jumbotronSection.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-// Import your images
-import gridImage1 from '../../assets/img-1.jpg';
-import gridImage2 from '../../assets/img-2.jpg';
-import gridImage3 from '../../assets/img-3.webp';
-import gridImage4 from '../../assets/img-4.jpg';
-import gridImage5 from '../../assets/img-5.jpg';
-import gridImage6 from '../../assets/img-7.jpg';
 import { Button } from '../button';
 import { Container } from '../container';
 
@@ -15,8 +8,15 @@ export const JumbotronSection = () => {
     const imgStyle = 'mb-6 h-[150px] w-[319.25px] rounded-3xl object-cover md:h-[300px]'
 
     return (
-        <section className='bg-darkBlue w-full h-[916.31px] flex items-center justify-center overflow-hidden md:h-screen'>
-           
+        <section className='w-full h-[916.31px] flex items-center justify-center md:h-screen relative'>
+            <div className="h-full relative">
+                <img
+                    className='rounded-4xl p-3 w-screen h-full object-cover'
+                    src="/hero.jpg"
+                    alt=""
+                />
+                <div className="absolute inset-0 bg-black/55 rounded-3xl m-3 "></div>
+            </div>
         </section>
     );
 };
