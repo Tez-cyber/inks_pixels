7681ebe293e6cd53eea591bf825f40482d64303c
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-21 00:23:55 +0100

    Added a span desc in hero-section

--- src/components/landing/jumbotronSection.jsx
+++ src/components/landing/jumbotronSection.jsx
@@ -5,26 +5,47 @@ import { Button } from '../button';
 import { Container } from '../container';
 
 export const JumbotronSection = () => {
-    const imgStyle = 'mb-6 h-[150px] w-[319.25px] rounded-3xl object-cover md:h-[300px]'
-
+    let spanArray = [];
+    for (let i = 1; i < 4; i++) {
+        spanArray.push(i)
+    }
     return (
-        <section className='relative w-full h-[916.31px] flex md:h-screen'>
-            <div className="h-full relative">
-                {/* Hero image */}
-                <img
-                    className=' w-screen h-full object-cover'
-                    src="/products_assets/jotter.jpg"
-                    alt=""
-                />
-                <div className="absolute inset-0 bg-black/70 bg-opacity-50"></div>
+        // <section className='relative w-full h-[916.31px] flex md:h-screen'>
+        //     <div className="h-full relative">
+        //         {/* Hero image */}
+        //         <img
+        //             className=' w-screen h-full object-cover'
+        //             src="/products_assets/jotter.jpg"
+        //             alt=""
+        //         />
+        //         <div className="absolute inset-0 bg-black/70 bg-opacity-50"></div>
+        //     </div>
+        //     {/* Title */}
+        //     <section className="absolute p-10 top-[20%]">
+        //         <h1 className="text-[28px] text-white z-3 leading-[1.3] md:text-[36px] lg:text-[50px]">
+        //             Empowering Your Brand <br className='hidden md:block' /> with
+        //             <span className='ml-3 text-veryLightBlue bg-darkBlue rounded-lg px-2'>Creative Excellence</span>
+        //         </h1>
+        //     </section>
+        // </section>
+        <section className='bg-veryLightBlue'>
+            <div className='h-screen'>
+                <Container className="pt-[10%] flex flex-col items-center">
+                    {/* Top span */}
+                    <div className='bg-darkBlue w-fit text-veryLightBlue rounded-2xl py-1 px-3 flex items-center gap-2'>
+                        <div className='flex'>
+                            {
+                                spanArray.map((_, i) => (
+                                    <span className='w-5 h-5 bg-veryLightBlue block rounded-full border-2 border-solid border-darkBlue'></span>
+                                ))
+                            }
+                        </div>
+                        <p>
+                            Built 100+ Personal Brands
+                        </p>
+                    </div>
+                </Container>
             </div>
-            {/* Title */}
-            <section className="absolute p-10 top-[20%]">
-                <h1 className="text-[28px] text-white z-3 leading-[1.3] md:text-[36px] lg:text-[50px]">
-                    Empowering Your Brand <br className='hidden md:block' /> with
-                    <span className='ml-3 text-veryLightBlue bg-darkBlue rounded-lg px-2'>Creative Excellence</span>
-                </h1>
-            </section>
         </section>
     );
 };
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -3,7 +3,7 @@
 export const Logo = () => {
     return (
         <section>
-            <h3 className="flex flex-col rounded-xl leading-tight px-5 text-lg">
+            <h3 className="flex text-white flex-col rounded-xl leading-tight px-5 text-lg">
                 Logo
             </h3>
         </section>
