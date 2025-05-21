0fc3c7ed075cfefe12054e89f88978b59b49400a
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-21 11:45:32 +0100

    Added main title in jumbotron and installed lenis

--- src/components/landing/jumbotronSection.jsx
+++ src/components/landing/jumbotronSection.jsx
@@ -1,52 +1,35 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
-import { Button } from '../button';
-import { Container } from '../container';
+import { Button } from "../button";
+import { Container } from "../container";
 
 export const JumbotronSection = () => {
-    let spanArray = [];
-    for (let i = 1; i < 4; i++) {
-        spanArray.push(i)
-    }
-    return (
-        // <section className='relative w-full h-[916.31px] flex md:h-screen'>
-        //     <div className="h-full relative">
-        //         {/* Hero image */}
-        //         <img
-        //             className=' w-screen h-full object-cover'
-        //             src="/products_assets/jotter.jpg"
-        //             alt=""
-        //         />
-        //         <div className="absolute inset-0 bg-black/70 bg-opacity-50"></div>
-        //     </div>
-        //     {/* Title */}
-        //     <section className="absolute p-10 top-[20%]">
-        //         <h1 className="text-[28px] text-white z-3 leading-[1.3] md:text-[36px] lg:text-[50px]">
-        //             Empowering Your Brand <br className='hidden md:block' /> with
-        //             <span className='ml-3 text-veryLightBlue bg-darkBlue rounded-lg px-2'>Creative Excellence</span>
-        //         </h1>
-        //     </section>
-        // </section>
-        <section className='bg-veryLightBlue'>
-            <div className='h-screen'>
-                <Container className="pt-[10%] flex flex-col items-center">
-                    {/* Top span */}
-                    <div className='bg-darkBlue w-fit text-veryLightBlue rounded-2xl py-1 px-3 flex items-center gap-2'>
-                        <div className='flex'>
-                            {
-                                spanArray.map((_, i) => (
-                                    <span className='w-5 h-5 bg-veryLightBlue block rounded-full border-2 border-solid border-darkBlue'></span>
-                                ))
-                            }
-                        </div>
-                        <p>
-                            Built 100+ Personal Brands
-                        </p>
-                    </div>
-                </Container>
+  let spanArray = [];
+  for (let i = 1; i < 4; i++) {
+    spanArray.push(i);
+  }
+  return (
+    <section className="bg-veryLightBlue">
+      <div className="h-screen">
+        <Container className="pt-[10%] flex flex-col items-center">
+          {/* Top span */}
+          <div className="bg-darkBlue w-fit text-veryLightBlue rounded-2xl py-1 px-3 flex items-center gap-2">
+            <div className="flex">
+              {spanArray.map((_, i) => (
+                <span className="w-5 h-5 bg-veryLightBlue block rounded-full border-2 border-solid border-darkBlue" />
+              ))}
             </div>
-        </section>
-    );
-};
+            <p>Built 100+ Personal Brands</p>
+          </div>
 
+          {/* Main Title */}
+          <div className="mt-10 text-7xl font-semibold leading-[1.4] text-center">
+            <p className="text-gray-500">Empowering Your Brand with</p>
+            <p className="text-darkBlue">Creative Excellence</p>
+          </div>
+        </Container>
+      </div>
+    </section>
+  );
+};
