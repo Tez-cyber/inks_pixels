8273f56811cfecbafe9876a72ab75db3a3b064bf
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-24 00:13:07 +0100

    Created ProductsComponent

---
+++ src/components/products/productsComponent.jsx
@@ -0,0 +1,47 @@
+import { Button } from "../button";
+
+export const ProductsComponent = () => {
+  return (
+    <section className="h-screen bg-veryLightBlue md:pt-[20%] lg:pt-[5%]">
+      {/* Products top section */}
+      <header className="bg-lightBlue/20 py-10 flex items-center justify-between sticky top-0 px-20">
+        {/* First section */}
+        <div>
+          <p>
+            Home
+            <span className="text-darkBlue font-bold"> | Products</span>
+          </p>
+          <p className="text-4xl font-semibold leading-[1.3]">
+            Explore All Products
+          </p>
+        </div>
+        {/* Second section */}
+        <div className="flex items-center">
+          <input
+            className="w-[350px] h-[20px] py-5 px-4 rounded-lg ml-10 outline-0 bg-veryLightBlue"
+            type="search"
+            placeholder="Search for products..."
+          />
+          <Button size="sm" variant="darkPrimary" className="mx-2 w-fit">
+            Search
+          </Button>
+        </div>
+      </header>
+      {/* Products display */}
+      <main>
+        <ProductsMain />
+      </main>
+    </section>
+  );
+};
+
+
+const ProductsMain = () => {
+
+    return (
+        <section>
+            <div className=""></div>
+            <div className=""></div>
+        </section>
+    )
+}
