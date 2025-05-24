fa4be30c3bf19c4bc8d807278bdff80192e648d2
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-24 23:36:27 +0100

    Added ProductsGrid component

--- src/components/products/productsComponent.jsx
+++ src/components/products/productsComponent.jsx
@@ -87,24 +87,29 @@ const ProductsDisplay = () => {
       </div>
 
       {/* Products grid */}
-      <div className="flex-1 p-5">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-          {products.map((product) => (
-            <div key={product.id} className="">
-              <img
-                src={product.img}
-                alt={product.title}
-                className="h-[200px] rounded-2xl w-full object-cover transition-transform duration-500"
-              />
-              <div className="p-4">
-                <h2 className="text-darkBlue font-semibold">{product.title}</h2>
-                <p className="text-sm">&#8358; {product.price}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <ProductsGrid />
     </div>
   );
 };
 
+const ProductsGrid = () => {
+  return (
+    <div className="flex-1 p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {products.map((product) => (
+          <div key={product.id} className="">
+            <img
+              src={product.img}
+              alt={product.title}
+              className="h-[200px] rounded-2xl w-full object-cover transition-transform duration-500"
+            />
+            <div className="p-4">
+              <h2 className="text-darkBlue font-semibold">{product.title}</h2>
+              <p className="text-sm">&#8358; {product.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
